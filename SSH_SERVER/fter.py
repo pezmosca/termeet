5d0e65bfb2460dfb1d8bc33bc3a5f7b6363fc6dc
EliaPrincesaSirena <iria.cervello23@gmail.com>
2018-10-21 03:19:46 +0200

    All FrontEnd Done

--- SSH_SERVER/fter.py
+++ SSH_SERVER/fter.py
@@ -13,11 +13,14 @@ def MainMenu():
     userID = "Pedrito"
     global profile
     profile = ProfileClass.Profile()
+
+    print "Welcome to TerMeet "
     f = open('termita.txt', 'r')
     mensaje = f.read()
     print(mensaje)
     f.close()
-    print "Welcome to TerMeet \nWhat are you seeking for?:\n"
+    print
+    print "What are you seeking for?:"
     print "1. Couple"
     print "2. HackBuddy"
     print "3. Job"
@@ -80,7 +83,7 @@ def printMenuTools(SubMenu):
 def ProfileMenu():
     #profile = CAPAMEDIA.getProfile(userID,subMenuSelected)
     print "What do you wanna do?:"
-    print "1. View Profile"
+    print "1. View my Profile"
     print "2. Add Interest"
     print "3. Edit Description"
     print "4. Edit Age"
@@ -170,6 +173,7 @@ def ViewProfil(profile):
     print "Nick: " + profile.nick
     print "Full Name: " + profile.fullName
     print "Interests: " + profile.interests
+    print "Description: " + profile.description
     print "Age: " + str(profile.age)
     print "Gender: " + profile.gender
     searching = ["None", "Others", "Boys", "Boys & Others", "Girls", "Girls & Others", "Girls & Boys",
@@ -199,22 +203,23 @@ def AddInterest():
     print
 
     entrada = int(input())
-    nuevosIntereses = profile.interests.split("/")
-    nuevosIntereses2 = []
-    nuevosIntereses.append("/")
-
-    while entrada != 0:
-        nuevosIntereses.append(intereses[int(entrada)-1])
-        profile.interests = profile.interests  + intereses[int(entrada)-1] + "/"
-        nuevosIntereses2.append(int(entrada))
+    if entrada < len(intereses):
+        nuevosIntereses = profile.interests.split("/")
+        nuevosIntereses2 = []
         nuevosIntereses.append("/")
-        for ni in nuevosIntereses:
-            print ni,
-        print
-        entrada = input()
 
-    #CAPAMEDIA.addTags(userID, nuevosIntereses2)
+        while entrada != 0:
+            nuevosIntereses.append(intereses[int(entrada)-1])
+            profile.interests = profile.interests  + intereses[int(entrada)-1] + "/"
+            nuevosIntereses2.append(int(entrada))
+            nuevosIntereses.append("/")
+            for ni in nuevosIntereses:
+                print ni,
+            print
+            entrada = input()
 
+        #CAPAMEDIA.addTags(userID, nuevosIntereses2)
+    else: print "There's no number " + entrada
     ProfileMenu()
 
 
@@ -222,31 +227,63 @@ def InboxMenu():
     print SubMenuTitle + "/Inbox"
     # matches[] = CAPAMEDIA.getMatches(userID)
     match = ProfileClass.Profile()
-    matches = [match, match, match]
+    match2 = ProfileClass.Profile()
+    match3 = ProfileClass.Profile()
+    match.nick = "ABBy"
+    match2.nick = "JeNNy"
+    match3.nick = "ANNa"
+    matches = [match, match2, match3]
     print "Matches:"
     for m in matches:
         print m.nick + ", " + m.fullName + ", " + m.correo
 
     # superlikes[] = CAPAMEDIA.getSuperlikes(userID)
-
-    print "If you want to see more about a match, press 1.\n If you want to come back to menu, press 2."
+    print
+    print "If you want to see more about a match, press 1.\nIf you want to come back to menu, press 2."
     aux=input()
     if aux == 1:
         print "Write your posible love name: "
-        nick = input()
+        nickLove = str(raw_input())
         b = False;
         for m in matches:
-            if m.nick == nick:
+            if m.nick == nickLove:
+                print
                 ViewProfil(m)
                 b = True;
         if not b:
-            print "There's no person called " + nick
-    else:
-        ExitProfileMenu()
+            print "There's no person called " + nickLove
+
+    ExitProfileMenu()
 
 
 def TermeetingMenu():
     print SubMenuTitle + "/Termeeting"
+    # candidatos[] = CAPAMEDIA.getCandidatos(userID)
+    match = ProfileClass.Profile()
+    match2 = ProfileClass.Profile()
+    match3 = ProfileClass.Profile()
+    match.nick = "ABBy"
+    match2.nick = "JeNNy"
+    match3.nick = "ANNa"
+    candidatos = [match, match2, match3]
+    for c in candidatos:
+        print "Do you like what you see?"
+        print
+        print "Image"
+        print "Nick: " + c.nick
+        print "Name: " + c.fullName
+        print
+        print "Use S To accept, A to Refuse, W To SuperMeet, Z to see more about "
+        arrow = raw_input()
+        if arrow == 'Z' or arrow == 'z':
+            ViewProfil(c)
+        elif arrow =='S' or arrow == 's':
+            #accept = CAPAMEDIA.likeFromTo(userID, c.userID)
+            accept = True
+            if accept ==True:
+                print "MATCH!"
+    print "Oh! There are not more people for you!"
+
 
 
 MainMenu()
